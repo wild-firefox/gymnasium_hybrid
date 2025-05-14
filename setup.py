54a5b518edbc08fe65784aaf904291ac9cdde15a
Jordan <jiadonghe007@outlook.com>
2025-05-14 20:28:17 +0800

    add libs needed

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 
 setup(
-    name='gymnasium_hybrid',
-    version='0.0.1',  # original gym_hybrid version='0.0.1'
-    packages=['gymnasium_hybrid'],
-    install_requires=['gymnasium>=1.0.0', 
-                      'numpy',
-                      'pygame>=2.3.0'
-                      ],
+    name="gymnasium_hybrid",
+    version="0.0.1",  # original gym_hybrid version='0.0.1'
+    packages=["gymnasium_hybrid"],
+    include_package_data=True,
+    package_data={
+        "gymnasium_hybrid": ["*.png", "*.jpg", "assets/*"],
+    },
+    install_requires=[
+        "gymnasium>=1.0.0",
+        "numpy",
+        "pygame>=2.3.0",
+        "opencv-python",
+        "moviepy",
+    ],
 )
